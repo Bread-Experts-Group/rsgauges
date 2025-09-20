4c74da892e39dba067f7c077bd4efb92cab40444
ttttdoy <logan3mclean@gmail.com>
2025-09-19 23:26:07 -0400

    populate repo with mod skeleton

---
+++ src/main/kotlin/org/bread_experts_group/rsgauges/RsGauges.kt
@@ -0,0 +1,58 @@
+package org.bread_experts_group.rsgauges
+
+import org.bread_experts_group.rsgauges.block.ModBlocks
+import net.neoforged.bus.api.IEventBus
+import net.neoforged.bus.api.SubscribeEvent
+import net.neoforged.fml.ModContainer
+import net.neoforged.fml.common.Mod
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
+import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+/**
+ * Main mod class.
+ */
+@Mod(RsGauges.ID)
+class RsGauges(eventBus: IEventBus, container: ModContainer) {
+    companion object {
+        const val ID = "rsgauges"
+    }
+
+    // the logger for our mod
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    init {
+        LOGGER.log(Level.INFO, "Hello world!")
+
+        // Register the KDeferredRegister to the mod-specific event bus
+        ModBlocks.REGISTRY.register(eventBus)
+
+        eventBus.addListener(this::onClientSetup)
+        eventBus.addListener(this::onServerSetup)
+        eventBus.addListener(this::onCommonSetup)
+    }
+
+    /**
+     * This is used for initializing client specific
+     * things such as renderers and keymaps
+     * Fired on the mod specific event bus.
+     */
+    private fun onClientSetup(event: FMLClientSetupEvent) {
+        LOGGER.log(Level.INFO, "Initializing client...")
+    }
+
+    /**
+     * Fired on the global Forge bus.
+     */
+    private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
+        LOGGER.log(Level.INFO, "Server starting...")
+    }
+
+    @SubscribeEvent
+    fun onCommonSetup(event: FMLCommonSetupEvent) {
+        LOGGER.log(Level.INFO, "Hello! This is working!")
+    }
+}
